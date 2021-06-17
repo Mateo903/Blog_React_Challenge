c50dd272ad04a4d4544515b7fe3588408cf2bac4
Mateo Gabriel Rios <mateo903@gmail.com>
2021-06-17 15:39:36 -0300

    "Installing react router"

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import React from 'react';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+} from "react-router-dom";
+import Home from './Home'
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/home" component={Home} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
