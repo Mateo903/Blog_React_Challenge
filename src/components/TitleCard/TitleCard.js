9e887f5df60f80bcb125b898a45f429481c4d3c6
Mateo Gabriel Rios <mateo903@gmail.com>
2021-06-17 20:31:47 -0300

    Stylling title card

--- src/components/TitleCard/TitleCard.js
+++ src/components/TitleCard/TitleCard.js
@@ -12,9 +12,9 @@ const TitleCard = (props) => {
     <div className='title-card-container'>
       <div className='title-container'><p>{props.title}</p></div>
       <div className='buttons-container'>
-        <a href='http://www.google.com' className='delete-button'><DeleteIcon/></a>
-        <a className='edit-button'><EditIcon/></a>
-        <a className='arrow-button'><ArrowForwardIcon/></a>
+        <a href='http://www.google.com' target="_blank" className='delete-button' rel="noreferrer"><DeleteIcon/></a>
+        <a href='http://www.google.com' target="_blank" className='edit-button' rel="noreferrer"><EditIcon/></a>
+        <a href='http://www.google.com' target="_blank" className='arrow-button' rel="noreferrer"><ArrowForwardIcon/></a>
       </div>
     </div>
   )
