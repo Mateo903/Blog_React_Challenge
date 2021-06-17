adcdc08acbafa8c474a41f0e26c9a0a5dad1e223
Mateo Gabriel Rios <mateo903@gmail.com>
2021-06-17 20:57:00 -0300

    Creating Post Detail Container

---
+++ src/containers/PostDetail/PostDetail.js
@@ -0,0 +1,32 @@
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
+import { useParams } from 'react-router'
+
+const PostDetail = () => {
+  const [data, setData] = useState({})
+  const {id} = useParams()
+  const APIUrl = 'https://jsonplaceholder.typicode.com/posts'
+
+  const reqAPI = async() =>{
+    const req = await axios({
+        method: 'GET', 
+        url: `${APIUrl}/${id}`,
+      })
+
+    setData(req.data)
+    console.log(req.data)
+  }
+
+  useEffect(()=>{
+    reqAPI()
+  },[])
+
+  return (
+    <div>
+      <h1>{data.title}</h1>
+      <p>{data.body}</p>
+    </div>
+  )
+}
+
+export default PostDetail
