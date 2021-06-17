d3c54001666dcad964201cf0ecb314667db052dd
Mateo Gabriel Rios <mateo903@gmail.com>
2021-06-17 16:59:33 -0300

    accessing the api placeholder with axios

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,36 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
+
 
 const Home = () => {
+  const APIUrl = 'https://jsonplaceholder.typicode.com/posts'
+
+  const [data, setData] = useState([])
+
+  const reqAPI = async() =>{
+    const req = await axios({
+        method: 'GET', 
+        url: APIUrl,
+      })
+
+    setData(req.data)
+    console.log(req.data)
+  }
+
+  useEffect(()=>{
+    reqAPI()
+  },[])
+
   return (
     <div>
-      <h1>HOLA MUNDO DESDE HOME</h1>
+      {data.map( blog => {
+        return(
+        <div>
+          <h4>{blog.title}</h4>
+          <hr/>
+        </div>
+        )
+      })}
     </div>
   )
 }
