ff2efc2d0432c2535100a3a56e5104096876dcfa
Mateo Gabriel Rios <mateo903@gmail.com>
2021-06-18 02:52:10 -0300

    Creating Edit Post Container

---
+++ src/containers/EditPost/EditPost.js
@@ -0,0 +1,56 @@
+import React, {useState, useEffect} from 'react'
+import CreatePost from '../CreatePost/CreatePost'
+import { useParams } from 'react-router'
+import axios from 'axios'
+
+const EditPost = () => {
+  const [data, setData] = useState({})
+  const {id} = useParams()
+  const APIUrl = 'https://jsonplaceholder.typicode.com/posts'
+
+  const reqAPI = async() =>{
+    const req = await axios({
+        method: 'GET', 
+        url: `${APIUrl}/${id}`,
+      })
+
+    setData(req.data)
+  }
+
+  useEffect(()=>{
+    reqAPI()
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  },[id])
+
+  const conditionalRender = (n) => {
+    if(n!== undefined){
+      return(
+        <CreatePost 
+        title='Edit the Post'
+        method='PUT'        
+        contentValue={data.body}
+        titleText={data.title}
+        buttonText='Edit the Post'
+        urlId={`${APIUrl}/${id}`}
+      />
+      )
+    }
+  }
+
+
+  return (
+    <div>
+      {conditionalRender(data.title)}
+      {/* <CreatePost 
+        title='Edit the Post'
+        method='PUT'        
+        contentValue={data.body}
+        titleText={data.title}
+        buttonText='Edit the Post'
+        id={id}
+      /> */}
+    </div>
+  )
+}
+
+export default EditPost
